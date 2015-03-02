f12cedd954dd3310d9a3e61edf593f49b24f96de
redpois0n <git@redpois0n>
2015-03-02 20:16:45 +0100

    Copy entry, check if detected

--- src/com/redpois0n/jstrip/ArchiveRewriter.java
+++ src/com/redpois0n/jstrip/ArchiveRewriter.java
@@ -3,17 +3,24 @@ package com.redpois0n.jstrip;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 public class ArchiveRewriter {
 	
 	private File input;
 	private File output;
+	private List<String> allowedClasses;
 	
-	public ArchiveRewriter(File file) {
+	public ArchiveRewriter(File file, List<String> allowedClasses) {
 		this.input = file;
 		this.output = file;
+		this.allowedClasses = allowedClasses;
 	}
 	
 	public boolean isSameFile() {
@@ -30,10 +37,39 @@ public class ArchiveRewriter {
 		}
 		
 		ZipOutputStream zos = new ZipOutputStream(os);
-	
+		ZipFile zip = new ZipFile(input);	
 		
-	
+		Enumeration<? extends ZipEntry> entries = zip.entries();
+		while (entries.hasMoreElements()) {
+			ZipEntry entry = entries.nextElement();
+			
+			if (shouldWrite(entry)) {
+				InputStream is = zip.getInputStream(entry);
+				zos.putNextEntry(entry);
+				
+				byte[] buffer = new byte[1024];
+				int read;
+				
+				while ((read = is.read(buffer)) != -1) {
+					zos.write(buffer, 0, read);
+				}
+				
+				is.close();
+				zos.closeEntry();
+			}		
+		}
+		
+		zip.close();
 		zos.close();
+	}
+	
+	public boolean shouldWrite(ZipEntry entry) {
+		String className = entry.getName().replace("/", ".");
+		if (className.startsWith(".")) {
+			className = className.substring(1, className.length());
+		}
+		
+		return allowedClasses.contains(className);
 	}
 
 }
