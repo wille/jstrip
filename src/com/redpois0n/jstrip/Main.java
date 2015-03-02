74078d8e4713efd6f80edb4f27994683acb70d26
redpois0n <git@redpois0n>
2015-03-02 09:17:20 +0100

    Arg methods

--- src/com/redpois0n/jstrip/Main.java
+++ src/com/redpois0n/jstrip/Main.java
@@ -5,5 +5,25 @@ public class Main {
 	public static void main(String[] args) {
 
 	}
+	
+	public static boolean argsContains(String[] args, String arg) {
+		for (String s : args) {
+			if (arg.equals(s)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static String getArg(String[] args, String arg) {
+		for (int i = 0; i < args.length; i++) {
+			if (arg.equals(args[i])) {
+				return args[i + 1];
+			}
+		}
+		
+		return null;
+	}
 
 }
