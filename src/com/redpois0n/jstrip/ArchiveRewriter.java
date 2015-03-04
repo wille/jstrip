cf4cfb9374136cc536fd01b5f05f99afaed03e30
redpois0n <git@redpois0n>
2015-03-04 07:56:16 +0100

    getTime(), returns time taken to complete action

--- src/com/redpois0n/jstrip/ArchiveRewriter.java
+++ src/com/redpois0n/jstrip/ArchiveRewriter.java
@@ -12,43 +12,46 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 public class ArchiveRewriter {
-	
+
 	private File input;
 	private File output;
 	private List<String> allowedClasses;
-	
+	private long start;
+	private long end;
+
 	public ArchiveRewriter(File input, File output, List<String> allowedClasses) {
 		this.input = input;
 		this.output = output;
 		this.allowedClasses = allowedClasses;
+		this.start = System.currentTimeMillis();
 	}
-	
+
 	public boolean isSameFile() {
 		return input == output;
 	}
-	
+
 	public void rewrite() throws Exception {
 		OutputStream os;
-		
+
 		if (isSameFile()) {
 			os = new ByteArrayOutputStream();
 		} else {
 			os = new FileOutputStream(output);
 		}
-		
+
 		ZipOutputStream zos = new ZipOutputStream(os);
-		ZipFile zip = new ZipFile(input);	
-		
+		ZipFile zip = new ZipFile(input);
+
 		Enumeration<? extends ZipEntry> entries = zip.entries();
 		while (entries.hasMoreElements()) {
 			ZipEntry entry = entries.nextElement();
-			
+
 			boolean isClass = Utils.isClass(entry.getName());
-			
+
 			if (shouldWrite(entry)) {
 				InputStream is = zip.getInputStream(entry);
 				zos.putNextEntry(entry);
-				
+
 				if (isClass) {
 					Main.log("Writing class " + entry.getName());
 				} else {
@@ -56,11 +59,11 @@ public class ArchiveRewriter {
 				}
 				byte[] buffer = new byte[1024];
 				int read;
-				
+
 				while ((read = is.read(buffer)) != -1) {
 					zos.write(buffer, 0, read);
 				}
-				
+
 				is.close();
 				zos.closeEntry();
 			} else if (isClass) {
@@ -69,14 +72,26 @@ public class ArchiveRewriter {
 				Main.log("Skipping " + entry.getName());
 			}
 		}
-		
+
 		zip.close();
 		zos.close();
+		
+		end = System.currentTimeMillis();
+		
+		if (isSameFile()) {
+			FileOutputStream fos = new FileOutputStream(output);
+			fos.write(((ByteArrayOutputStream) os).toByteArray());
+			fos.close();
+		}
 	}
 	
+	public long getTime() {
+		return end - start;
+	}
+
 	public boolean shouldWrite(ZipEntry entry) {
 		String className = Utils.getClassName(entry.getName());
-						
+
 		return Utils.isClass(entry.getName()) && allowedClasses.contains(className) || !Utils.isClass(entry.getName());
 	}
 
