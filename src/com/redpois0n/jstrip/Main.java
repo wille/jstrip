f7a9682ae394a30ddb5d16494510b32fc1cc586c
redpois0n <git@redpois0n>
2015-03-04 12:46:21 +0100

    -io arg removed

--- src/com/redpois0n/jstrip/Main.java
+++ src/com/redpois0n/jstrip/Main.java
@@ -15,34 +15,24 @@ public class Main {
 			File outDir = null;
 			String mainClass;
 
-			if (argsContains(args, "-io")) {
-				String sjar = getArg(args, "-io");
-				if (sjar != null) {
-					launchJar = new File(sjar);
-					outDir = new File(sjar);
-				} else {
-					throw new IllegalArgumentException("No file specified");
-				}
-			} else {
-				if (argsContains(args, "-in")) {
-					String sLaunchJar = getArg(args, "-in");
-					if (sLaunchJar != null) {
-						launchJar = new File(sLaunchJar);
-					} else {
-						throw new IllegalArgumentException("Input file needs to be specified");
-					}
+			if (argsContains(args, "-in")) {
+				String sLaunchJar = getArg(args, "-in");
+				if (sLaunchJar != null) {
+					launchJar = new File(sLaunchJar);
 				} else {
 					throw new IllegalArgumentException("Input file needs to be specified");
 				}
-				
-				if (argsContains(args, "-out")) {
-					String sOutDir = getArg(args, "-out");
+			} else {
+				throw new IllegalArgumentException("Input file needs to be specified");
+			}
 
-					if (sOutDir != null) {
-						outDir = new File(sOutDir);
-					} else {
-						throw new IllegalArgumentException("Output directory not specified");
-					}
+			if (argsContains(args, "-out")) {
+				String sOutDir = getArg(args, "-out");
+
+				if (sOutDir != null) {
+					outDir = new File(sOutDir);
+				} else {
+					throw new IllegalArgumentException("Output directory not specified");
 				}
 			}
 
@@ -59,42 +49,42 @@ public class Main {
 					throw new IllegalArgumentException("Library not specified");
 				}
 			}
-			
+
 			if (argsContains(args, "-main")) {
 				mainClass = getArg(args, "-main");
 			} else {
 				throw new ClassNotFoundException("Main class not specified");
 			}
-			
+
 			List<JarInputStream> jiss = new ArrayList<JarInputStream>();
 			for (File file : libraries) {
 				Main.log("Loading library " + file.getName());
 				jiss.add(new JarInputStream(new FileInputStream(file)));
 			}
-			
+
 			jiss.add(new JarInputStream(new FileInputStream(launchJar)));
-			
+
 			Scanner scanner = new Scanner(mainClass, jiss, args);
 			scanner.run();
-			
+
 			for (File file : libraries) {
 				Main.log("Stripping library " + file.getName());
-				
+
 				File out = new File(outDir, file.getName());
-				
+
 				ArchiveRewriter writer = new ArchiveRewriter(file, out, scanner.getLoadedClasses());
 				writer.rewrite();
-				
+
 				Main.log("Stripped library " + file.getName() + ", Took " + writer.getTime() + " ms, Percent smaller " + writer.getSizeReduced() + "%, Old file size, " + DataUnits.getAsString(writer.getOldSize()) + ", New file size " + DataUnits.getAsString(writer.getNewSize()));
 			}
-			
+
 			Main.log("Completed, stripped " + libraries.size() + " libraries");
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			printUsage();
 		}
 	}
-	
+
 	public static void log(String s) {
 		System.out.println(s);
 	}
